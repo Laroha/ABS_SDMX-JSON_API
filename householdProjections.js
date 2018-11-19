e12079edff7bc66c4df7e1b4002e0f0d6666e08c
Laroha <45182677+Laroha@users.noreply.github.com>
2018-11-19 22:23:05 +0100

    Update householdProjections.js

--- householdProjections.js
+++ householdProjections.js
@@ -4,17 +4,42 @@
    myConnector.getSchema = function (schemaCallback) {
     
 	var cols = [
-        { id : "ASGS_2011_STATE_GCCSA_SA4_SA3_SA2", alias : "Region", dataType : tableau.dataTypeEnum.string },
-        { id : "HH_TYPE", alias : "Household Type", dataType : tableau.dataTypeEnum.string },
-        { id : "PROJ_SERIES", alias : "Projection Assumption", dataType : tableau.dataTypeEnum.string },
-        { id : "FREQUENCY", alias : "Frequency", dataType : tableau.dataTypeEnum.string },
-        { id : "TIME_PERIOD", alias : "Time", dataType : tableau.dataTypeEnum.string },
-        { id : "obs", alias : "observation", dataType : tableau.dataTypeEnum.float }
-    ];
+        { id : "stillingsnummer", alias : "Stillingnummer", dataType : tableau.dataTypeEnum.int },
+        { id : "stillingsnummer_nav_no", alias : "Stillingsnummer_NAV", dataType : tableau.dataTypeEnum.int },
+        { id : "stilling_id", alias : "Stillingsid", dataType : tableau.dataTypeEnum.int },
+        { id : "nav_enhet_kode", alias : "NAV enhet", dataType : tableau.dataTypeEnum.int },
+        { id : "registrert_dato", alias : "Registreringsdato", dataType : tableau.dataTypeEnum.date },
+        { id : "sistepubl_dato", alias : "Siste publiseringsdato", dataType : tableau.dataTypeEnum.date },
+        { id : "statistikk_aar_mnd", alias : "ARMANAD", dataType : tableau.dataTypeEnum.int },
+        { id : "offisiell_statistikk_flagg", alias : "Offisiell statistikk", dataType : tableau.dataTypeEnum.int },
+
+        { id : "stilling_kilde", alias : "Stillingskilde", dataType : tableau.dataTypeEnum.string },
+        { id : "arbeidssted_fylkesnummer", alias : "Fylkesnr", dataType : tableau.dataTypeEnum.int },
+        { id : "arbeidssted_fylke", alias : "Fylke", dataType : tableau.dataTypeEnum.string },
+        { id : "arbeidssted_kommunenummer", alias : "Kommunenr", dataType : tableau.dataTypeEnum.int },
+        { id : "arbeidssted_kommune", alias : "Kommune", dataType : tableau.dataTypeEnum.string },
+        { id : "arbeidssted_landkode", alias : "Landkode", dataType : tableau.dataTypeEnum.string },
+        { id : "arbeidssted_land", alias : "Land", dataType : tableau.dataTypeEnum.string },
+        { id : "isco_versjon", alias : "ISCO versjon", dataType : tableau.dataTypeEnum.string },
+        { id : "yrke_grovgruppe", alias : "Yrkesgruppe", dataType : tableau.dataTypeEnum.string },
+        { id : "yrkeskode", alias : "Yrkeskode", dataType : tableau.dataTypeEnum.int },
+        { id : "yrke", alias : "Yrke", dataType : tableau.dataTypeEnum.string },
+        { id : "yrkesbetegnelse", alias : "Yrkesbetegnelse", dataType : tableau.dataTypeEnum.string },
+        { id : "Bedrift Org Nr", alias : "Bedriftens organisasjonsnr", dataType : tableau.dataTypeEnum.int },
+        { id : "Bedrift Navn", alias : "Bedrift", dataType : tableau.dataTypeEnum.string },
+        { id : "Aktiv Flagg", alias : "Flagg", dataType : tableau.dataTypeEnum.string },
+        { id : "Bedrift Naring Primar Kode", alias : "Næringskode", dataType : tableau.dataTypeEnum.int },
+        { id : "Foretak Org Nr", alias : "Foretakets organisasjonsnr", dataType : tableau.dataTypeEnum.int },
+        { id : "Foretak Navn", alias : "Foretak", dataType : tableau.dataTypeEnum.string },
+        { id : "Foretak Sektor Gruppe", alias : "Sektor", dataType : tableau.dataTypeEnum.string },
+        { id : "Tilleggskriterium", alias : "Tilleggskriterium", dataType : tableau.dataTypeEnum.string },
+        { id : "antall_stillinger", alias : "Antall stillinger", dataType : tableau.dataTypeEnum.int },
+        { id : "stillingstittel", alias : "Stillingstittel", dataType : tableau.dataTypeEnum.string },
+   ];
 
     var tableInfo = {
-        id : "ABS_HH_PROV",
-        alias : "Household Projections, Australia, 2011 to 2036",
+        id : "Ledige_stillinger",
+        alias : "Ledige stillinger",
         columns : cols
     };
 
@@ -29,14 +54,14 @@ schemaCallback([tableInfo]);
  
     myConnector.getData = function (table, doneCallback) {
     var tableData = [],
-        ASGS_2011_STATE_GCCSA_SA4_SA3_SA2 = "",
+        Ledige_stillinger = "",
         HH_TYPE = "",
         PROJ_SERIES = "",
         Frequency = "",
         TIME_PERIOD = "",
         obs = 0;
 
-    	$.getJSON("http://stat.data.abs.gov.au/sdmx-json/data/ABS_HH_PROV/1+2+3+4+5+6+7+8.1_2+32+31.1+2+3.A/all?startTime=2011&endTime=2036&dimensionAtObservation=allDimensions", function (resp) {
+    	$.getJSON("https://data.nav.no/api/3/action/datastore_search", function (resp) {
         var obsvs = resp.dataSets[0].observations; 
 
 
